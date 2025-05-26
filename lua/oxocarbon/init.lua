5510bc9be9e1695bd31ed5356a24d1d11007e7c8
momeppkt <peeranut32@gmail.com>
2025-05-26 16:09:27 +0700

    fix: rename extra color name from `pink` to `red`

--- lua/oxocarbon/init.lua
+++ lua/oxocarbon/init.lua
@@ -12,7 +12,7 @@ local base09 = "#78a9ff"
 local oxocarbon = (((vim.o.background == "dark") and {base00 = base00, base01 = blend_hex(base00, base06, 0.085), base02 = blend_hex(base00, base06, 0.18), base03 = blend_hex(base00, base06, 0.3), base04 = blend_hex(base00, base06, 0.82), base05 = blend_hex(base00, base06, 0.95), base06 = base06, base07 = "#08bdba", base08 = "#3ddbd9", base09 = base09, base10 = "#ee5396", base11 = "#33b1ff", base12 = "#ff7eb6", base13 = "#42be65", base14 = "#be95ff", base15 = "#82cfff", blend = "#131313", none = "NONE"}) or {base00 = base06, base01 = blend_hex(base00, base06, 0.95), base02 = blend_hex(base00, base06, 0.82), base03 = base00, base04 = "#37474F", base05 = "#90A4AE", base06 = "#525252", base07 = "#08bdba", base08 = "#ff7eb6", base09 = "#ee5396", base10 = "#FF6F00", base11 = "#0f62fe", base12 = "#673AB7", base13 = "#42be65", base14 = "#be95ff", base15 = "#FFAB91", blend = "#FAFAFA", none = "NONE"})
 local pumblend = vim.opt.pumblend:get()
 local winblend = vim.opt.winblend:get()
-oxocarbon["pink"] = "#f38ba8"
+oxocarbon["red"] = "#f38ba8"
 oxocarbon["orange"] = "#ff6f00"
 oxocarbon["yellow"] = "#ffab91"
 oxocarbon["green"] = "#a6e3a1"
@@ -28,7 +28,7 @@ vim.api.nvim_set_hl(0, "NoiceCmdlineIconSearch", {link = "lualine_a_command"})
 vim.api.nvim_set_hl(0, "NoiceCmdlineIconFilter", {link = "lualine_a_command"})
 vim.api.nvim_set_hl(0, "NoiceSplit", {link = "Normal"})
 vim.api.nvim_set_hl(0, "NoiceSplitBorder", {link = "NoiceSplit"})
-vim.api.nvim_set_hl(0, "NoiceFormatDate", {fg = oxocarbon.pink})
+vim.api.nvim_set_hl(0, "NoiceFormatDate", {fg = oxocarbon.red})
 vim.api.nvim_set_hl(0, "NoiceFormatEvent", {fg = oxocarbon.orange})
 vim.api.nvim_set_hl(0, "NoiceFormatKind", {fg = oxocarbon.green})
 vim.api.nvim_set_hl(0, "SnacksPicker", {bg = oxocarbon.float_bg, blend = winblend})
@@ -245,7 +245,7 @@ vim.api.nvim_set_hl(0, "Tag", {fg = oxocarbon.base04, bg = oxocarbon.none})
 vim.api.nvim_set_hl(0, "Todo", {fg = oxocarbon.base13, bg = oxocarbon.none})
 vim.api.nvim_set_hl(0, "Type", {fg = oxocarbon.base09, bg = oxocarbon.none})
 vim.api.nvim_set_hl(0, "Typedef", {fg = oxocarbon.base09, bg = oxocarbon.none})
-vim.api.nvim_set_hl(0, "@markup.heading.1.markdown", {fg = oxocarbon.pink})
+vim.api.nvim_set_hl(0, "@markup.heading.1.markdown", {fg = oxocarbon.red})
 vim.api.nvim_set_hl(0, "@markup.heading.2.markdown", {fg = oxocarbon.orange})
 vim.api.nvim_set_hl(0, "@markup.heading.3.markdown", {fg = oxocarbon.yellow})
 vim.api.nvim_set_hl(0, "@markup.heading.4.markdown", {fg = oxocarbon.green})
